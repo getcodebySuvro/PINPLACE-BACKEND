65d1f7bc6fbd78fb3530aa5472ba3edf16210fb5
getcodebySuvro <suvrojyotibhuniya@gmail.com>
2022-08-07 22:12:50 +0530

    make it better

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json({limit:'50mb'}));
 
 
 //Mongoose Connection
-mongoose.connect(process.env.DB_URL,{
+mongoose.connect("mongodb+srv://pinplace:pinplace50@cluster0.iryrr.mongodb.net/pindb?retryWrites=true&w=majority",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
 }).then(()=>{
